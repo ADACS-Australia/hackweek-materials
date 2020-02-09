43caa819afbd8b57671ff33afaa9393a23226638
Pascal Jahan Elahi <pelahi@users.noreply.github.com>
2020-02-09 16:23:21 +0800

    Add files via upload

---
+++ hpc-pelahi/examples/bad/pthreads_datarace.cpp
@@ -0,0 +1,27 @@
+#include <pthread.h>
+#include <stdio.h>
+#define NT 10
+#define NI 100
+int Global, oldGlobal;
+void *Thread1(void *x) {Global++;return NULL;}
+void *Thread2(void *x) {Global--;return NULL;}
+int main() {
+  pthread_t t[NT];
+  oldGlobal=Global=2;
+  for (auto i=0;i<NI;i++) {
+    for (auto j=0;j<NT;j++) {
+        if (j<NT/2) pthread_create(&t[j], NULL, Thread2, NULL);
+        else pthread_create(&t[j], NULL, Thread1, NULL);
+        //if (Global<=oldGlobal) pthread_create(&t[j], NULL, Thread1, NULL);
+        //else pthread_create(&t[j], NULL, Thread2, NULL);
+        //pthread_join(t[j], NULL);
+    }
+    for (auto j=0;j<NT;j++) pthread_join(t[j], NULL);
+    if (Global!= oldGlobal) {
+      printf("Issue after %d iteration have %d instead of %d \n",i, Global, oldGlobal);
+      break;
+    }
+  }
+}
+
+
